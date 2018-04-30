896c9f0981f714d40186ff1b418b81ab45229974
백성빈 Baek Seongbin <sungbin0515@postech.ac.kr>
2018-05-01 02:04:41 +0900

    20180501(1)

--- Programming_Practice/BaekJoon/Python/2693.py
+++
@@ -1,4 +0,0 @@
-for i in range(int(input())):
-    l = list(map(int,input().split()))
-    l.sort()
-    print(l[7])
--- Programming_Practice/LAB05/TruthTable_Generate.py
+++
@@ -1,3 +0,0 @@
-for i in range(0,8):
-    for j in range(0,8):
-        print(bin(i+8)[3:],bin(j+8)[3:],bin(i*j+64)[3:])
