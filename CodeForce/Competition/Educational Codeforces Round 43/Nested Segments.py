413fd37c5b72fe371af99c2e9bc88aaddee90a38
P17seongbin <sungbin0515@postech.ac.kr>
2018-05-01 13:42:09 +0900

    20180501

--- CodeForce/Competition/Educational Codeforces Round 43/Nested Segments.py
+++ CodeForce/Competition/Educational Codeforces Round 43/Nested Segments.py
@@ -1,34 +1,37 @@
 #Failed
-n = int(input())
-
 def divconq(seq):
     if(len(seq) <= 1):
         return False
+
+    eraser = seq[len(seq)//2]
+    skip_index = len(seq)//2
+
     
-    eraser = seq[0]
-    left = []
-    l_c = 0
-    right  = []
-    r_c = 0
-    for i in range(1,len(seq)):
-        if (seq[i][0] <= eraser[0] and seq[i][1] >= eraser[1]) or (seq[i][0] >= eraser[0] and seq[i][1] <= eraser[1]):
-            print(seq[i][2],eraser[2])
-            return True
-        elif (seq[i][0] < eraser[0]):
-            left.append(seq[i])
-            left[l_c][1] = min(left[l_c][1],eraser[0])
-            l_c += 1
-        elif (seq[i][1] > eraser[1]):
-            right.append(seq[i])
-            right[r_c][0] = max(right[r_c][0],eraser[1])
-            r_c += 1
-    return divconq(left) or divconq(right)
+    if divconq(seq[:skip_index]) or divconq(seq[skip_index+1:]):
+        return True
+    else:
+        for i in range(skip_index):
+            if(seq[i][1] >= eraser[1]):
+                print(eraser[2],seq[i][2])
+                return True
+        for i in range(skip_index+1,len(seq)):
+            if(seq[i][1] <= eraser[1]):
+                print(seq[i][2],eraser[2])
+                return True
+
+n = int(input())
+
 
     
 seq = []
 for i in range(n):
-    seq.append(list(map(int,input().split())))
-    seq[i].append(i+1)#Index
+    #seq.append(list(map(int,input().split())))
+    seq.append([i,i])
+    seq[i].append(i+1)
+seq = sorted(seq,key = lambda x : (x[0]))
+
+
+
 
 if not divconq(seq):
     print(-1,-1)
